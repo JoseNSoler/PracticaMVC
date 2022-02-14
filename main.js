9c21de401af081c3fdc4f45130443c06ed037c6f
JoseNSoler <josenico1212@gmail.com>
2022-02-14 09:22:43 -0600

    [V1.0] creacion canvas

---
+++ main.js
@@ -0,0 +1,39 @@
+(function(){
+    self.Board = function(width, height){
+        this.width = width;
+        this.height = height;
+        this.playing = false;
+        this.game_over = false;
+        this.bars = [];
+        this.ball = null;
+    }
+
+    self.Board.prototype = {
+        get elements(){
+            var elements = this.bars;
+            elements.push(ball);
+            return elements;
+        }
+    }
+})();
+
+(function(){
+    self.BoardView = function(canvas, board){
+        this.canvas = canvas;
+        this.canvas.width = board.width;
+        this.canvas.height = board.height;
+        this.board = board;
+        this.context = canvas.getContext("2d");
+    }
+})();
+
+self.addEventListener("load", main);
+
+
+function main(){
+    console.log("Hola mundo");
+    var board = new Board(700, 400);
+    var canvas = document.getElementById('canvas', board);
+    var board_view = new BoardView(canvas, board);
+    
+}
