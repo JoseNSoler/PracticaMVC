248d2f998957d7573bb833f91062d9aac2609e5e
JoseNSoler <josenico1212@gmail.com>
2022-02-19 23:11:22 -0600

    [v2.1] Documentacion por comentarios en el archivo main.js - Instrucciones de jueg en index.html

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+/** Funcion main - Creacion de un tablero=Board y sus metodos para agregar y ver objetos
+ */
+
 (function(){
     // Inicializacion de un board de anchura y altura
     self.Board = function(width, height){
@@ -68,6 +71,7 @@
 })();
 
 (function(){
+    // Inicializacion de una barra en posiciones x-y, altura anchura y el tablero-board destino
     self.Bar = function(x, y, width, height, board){
         this.x = x;
         this.y = y;
@@ -77,14 +81,17 @@
         this.kind = "rectangle";
         this.speed = 10;
     }
-
+    // Fuciones por defecto de la barra
     self.Bar.prototype = {
+        // Abajo ⇃, aumenta la posicion en Y en el tablero en base a velocidad
         down: function(){
             this.y += this.speed;
         },
+        // Arriba ↾, decerementa la posicion en Y en el tablero en base a velocidad
         up: function(){
             this.y -= this.speed;
         },
+        // Metodo para devolver coordenadas en formato String
         toString: function(){
             return "x "+ this.x + " y: "+this.y;
         }
@@ -94,6 +101,7 @@
 
 
 (function(){
+    // Inicializa el controlador para la vista de un tablero, basado en un canvas
     self.BoardView = function(canvas, board){
         this.canvas = canvas;
         this.canvas.width = board.width;
@@ -101,27 +109,33 @@
         this.board = board;
         this.context = canvas.getContext("2d");
     }
-
+    // Fuciones por defecto de la barra
     self.BoardView.prototype = {
-
+        // clean y draw, funcionan como actualizadores de pantalla
+        // Elimina contenido de el tablero
         clean: function(){
             this.context.clearRect(0,0,this.canvas.width, this.canvas.height);
         },
+        // Actualiza contenido del tablero en base a elementos y sus propias coordenadas
         draw: function(){
             for (let i = this.board.elements.length - 1; i >= 0; i--) {
                 var element = this.board.elements[i];
                 draw(this.context, element);
             }
         },
+        // Revisa colisiones en los objetos de el tablero
         check_collisions: function(){
 
             for (let x = this.board.bars.length - 1; x >= 0; x--) {
                 var bar = this.board.bars[x];
+                // Si encuenta una colision en las barras con la bola
                 if(hit(bar, this.board.ball)){
+                    // Activa el metodo colision en bola para cambiar la direccion segun logica
                     this.board.ball.collision(bar);
                 }
             }
         },
+        // Inicializa principales movimientos segun si el usuario esta jugando o no -- Con tecla espacio se pausa
         play: function(){
             if(this.board.playing){
                 this.clean();
@@ -160,9 +174,8 @@
         return hit;
     }
 
-
+    // Metodo para dibujar elemento segun su forma
     function draw(context,element){
-
             switch(element.kind){
                 case "rectangle":
                     context.fillRect(element.x, element.y, element.width, element.height);
@@ -177,6 +190,11 @@
     }
 })();
 
+/** Declaracion de variables globales necesarias para un juego de ping pong -
+ * NOTA: Se necesitan dos jugares para controlar las barras
+ * bar = Jugador 1 = Teclas para jugar (w->arriba , s->abajo)
+ * bar2 = Jugador 2 = Teclas para jugar (Flecha arriba->arriba , Flecha abajo->abajo)
+ */
 var board = new Board(800, 400);
 var bar2 = new Bar(20, 100, 40, 100, board);
 var bar = new Bar(740, 100, 40, 100, board);
@@ -185,7 +203,7 @@ var canvas = document.getElementById('canvas', board);
 var board_view = new BoardView(canvas, board);
 
 
-
+// Metodos globales para recibir actividad de teclas presionadas 
 document.addEventListener("keydown", function(ev){
     console.log(ev.key)
     if(ev.key == "ArrowUp"){
@@ -210,10 +228,13 @@ document.addEventListener("keydown", function(ev){
     }
 });
 
+// Primer diagramado del tablero en canvas
 board_view.draw();
 
+// Controlador para mostrar animaciones por fragmentos estables
 window.requestAnimationFrame(controller);
 
+// Funcion principal - inicio juego
 function controller(){
     board_view.play();
     window.requestAnimationFrame(controller);
