414ca7c82f3241fa2fe8c777ed59fc3073e606db
JoseNSoler <josenico1212@gmail.com>
2022-02-19 22:54:15 -0600

    [V2.0] juego Ping Pong funcional (no tiene en cuenta bordes)

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 (function(){
+    // Inicializacion de un board de anchura y altura
     self.Board = function(width, height){
         this.width = width;
         this.height = height;
@@ -10,6 +11,7 @@
     }
 
     self.Board.prototype = {
+        // elementos publicos que muestra el board
         get elements(){
             var elements = this.bars.slice( ()=>bar);
             elements.push(this.ball);
@@ -19,8 +21,8 @@
 })();
 
 (function(){
+    // Inicializacion bola en coord x -y , con radio y en un board
     self.Ball = function(x, y, radius, board){
-        
         this.x = x;
         this.y = y;
         this.radius = radius;
@@ -29,13 +31,38 @@
         this.speed_x = 3;
         this.board = board;
         this.direction = 1;
+        this.bounce_angle = 0;
+        this.max_bounce_angle = Math.PI / 12;
+        this.speed = 3;
         this.kind = "circle";
         board.ball = this;
     }
+    // metodos por defecto en bola
     self.Ball.prototype = {
+        // Mover bola
         move: function(){
             this.x += (this.speed_x * this.direction);
             this.y += (this.speed_y);
+        },
+        get width(){
+            return this.radius * 2;
+        },
+        get height(){
+            return this.radius * 2;
+        },
+        // Existe colision entre algun objeto y la bola
+        collision: function(bar){
+            var relative_intersect_y = (bar.y + (bar.height / 2) ) - this.y;
+
+            var normalized_intersect_Y = relative_intersect_y / (bar.height / 2);
+
+            this.bounce_angle = normalized_intersect_Y * this.max_bounce_angle;
+
+            this.speed_y = this.speed * -Math.sin(this.bounce_angle);
+            this.speed_x = this.speed * Math.cos(this.bounce_angle);
+
+            if(this.x > (this.board.width / 2)) this.direction = -1;
+            else this.direction = 1;
         }
     }
 })();
@@ -86,14 +113,53 @@
                 draw(this.context, element);
             }
         },
+        check_collisions: function(){
+
+            for (let x = this.board.bars.length - 1; x >= 0; x--) {
+                var bar = this.board.bars[x];
+                if(hit(bar, this.board.ball)){
+                    this.board.ball.collision(bar);
+                }
+            }
+        },
         play: function(){
             if(this.board.playing){
                 this.clean();
                 this.draw();
+                this.check_collisions();
                 this.board.ball.move();
-            } 
+            }
         }
     }
+
+    // Revisa colisiones en los objetos a o b
+    function hit(a, b){
+        var hit = false;
+
+        // Colision horizontal
+        if(b.x + b.width >= a.x && b.x < a.x + a.width){
+            // Colision vertical
+            if(b.y + b.height >= a.y && b.y < a.y + a.height){
+                hit = true;
+            }
+        }
+
+        // Colision de a con b
+        if(b.x <= a.x && b.x + b.width >= a.x + a.width){
+            if(b.y <= a.y && b.y + b.height >= a.y + a.height){
+                hit = true;
+            }
+        }
+
+        // Colision de b con a
+        if(a.x <= b.x && a.x + a.width >= b.x + b.width){
+            if(a.y <= b.y && a.y + a.height >= b.y + b.height){
+                hit = true;
+            }
+        }
+        return hit;
+    }
+
 
     function draw(context,element){
 
